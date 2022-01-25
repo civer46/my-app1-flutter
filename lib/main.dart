124cabb546afa9b9cae4765d96a433d33651934a
civer46 <civer.yoshioka@gmail.com>
2022-01-25 20:16:45 +0700

    tambah YA

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
- 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: MyHomePage(),
-      
     );
   }
 }
@@ -21,16 +19,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BelajarFlutter.com'),
       ),
-      body: const Center(
-        child: Text('Ini Text Pertama Saya')
-      ),
+      body: const Center(child: Text('Ini Text Pertama Ya')),
     );
   }
 }
